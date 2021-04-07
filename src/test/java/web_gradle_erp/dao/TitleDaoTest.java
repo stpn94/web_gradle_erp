14acd5d1d8f842a6e9b9ebcd3905c65ccf0ee893
jylee <stpn94@gmail.com>
2021-04-07 16:41:36 +0900

    title dao test(selectbyall)

--- src/test/java/web_gradle_erp/dao/TitleDaoTest.java
+++ src/test/java/web_gradle_erp/dao/TitleDaoTest.java
@@ -1,29 +1,57 @@
 package web_gradle_erp.dao;
 
-import static org.junit.Assert.*;
+import java.sql.Connection;
+import java.util.List;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 
+import web_gradle_erp.JdbcUtil;
+import web_gradle_erp.dao.impl.TitleDaoImpl;
+import web_gradle_erp.dto.Title;
+@FixMethodOrder
 public class TitleDaoTest {
-
+	private static TitleDaoImpl dao = TitleDaoImpl.getInstance();
+	private static Connection con;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception{
+		con= JdbcUtil.getConnection();
+		dao.setCon(con);
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		System.out.println();
+	}
+	
 	@Test
-	public void testSelectTitleByAll() {
+	public void test01SelectTitleByAll() {
+		System.out.printf("%s()%n", "testSelectTitleByAll");
+		List<Title> list = dao.selectTitleByAll();
+		Assert.assertNotNull(list);
+
+		list.stream().forEach(System.out::println);
+
 	}
 
 	@Test
-	public void testSelectTitleByNo() {
+	public void test02SelectTitleByNo() {
 	}
 
 	@Test
-	public void testInsertTitle() {
+	public void test03InsertTitle() {
 	}
 
 	@Test
-	public void testUpdateTitle() {
+	public void test04UpdateTitle() {
 	}
 
 	@Test
-	public void testDeleteTitle() {
+	public void test05DeleteTitle() {
 	}
 
 }
